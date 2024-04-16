6547a211f2806ed0215de50776fac00b03fa0874
Nicholas George <137228821+njgeorge000158@users.noreply.github.com>
2024-04-16 12:00:08 -0700

    Add files via upload

---
+++ student_loans_constants.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#*******************************************************************************************
+ #
+ #  File Name:  student_loans_constants.py
+ #
+ #  File Description:
+ #      This Python script, student_loans_constants.py, contains generic Python constants
+ #      for using deep learning for student loan default prediction.
+ #
+ #
+ #  Date            Description                             Programmer
+ #  ----------      ------------------------------------    ------------------
+ #  04/15/2024      Initial Development                     Nicholas J. George
+ #
+ #******************************************************************************************/
+
+
+# In[2]:
+
+
+CONSTANT_LOCAL_FILE_NAME = 'spam_detector_constants.py'
+
+
+# In[3]:
+
+
+CONSTANT_INPUT_FILE_PATH = 'https://static.bc-edx.com/mbc/ai/m6/datasets/student_loans.csv'
+
+CONSTANT_NN_MODEL_FILE_PATH = './resources/nn_student_loans_model.keras'
+
+CONSTANT_DL_RANDOM_STATE_1 = 21
+
+
+# In[ ]:
+
+
+
+
